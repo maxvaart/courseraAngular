7d5c9438ef1c0da8e405d3fc84d3c79cec9eff62
maxvaart <maximiliano_lain3@hotmail.com>
2020-12-02 16:39:07 -0300

    New Services Part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import {DishService} from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -45,7 +46,7 @@ import { ContactComponent } from './contact/contact.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
