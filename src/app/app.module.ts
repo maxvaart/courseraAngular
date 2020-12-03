4cfcf17396c7544000e55660e3e2ca3494d2c652
maxvaart <maximiliano_lain3@hotmail.com>
2020-12-03 20:05:30 -0300

    Login Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
-
-import { AppRoutingModule } from '../app/app-routing/app-routing.module';
-import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule } from '../app/app-routing/app-routing.module';
+import {AppComponent } from './app.component';
 
 
 import 'hammerjs';
@@ -49,7 +52,11 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
+    FormsModule
   ],
   entryComponents:[LoginComponent],
   providers: [DishService,PromotionService, LeadersService],
