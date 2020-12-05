99ac09416598c1b1befe5bf6793d3a60e1d8c048
maxvaart <maximiliano_lain3@hotmail.com>
2020-12-05 16:29:01 -0300

    Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule } from '../app/app-routing/app-routing.module';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {ReactiveFormsModule} from '@angular/forms';
 import {AppComponent } from './app.component';
 
 
@@ -56,7 +59,10 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule,
     MatCheckboxModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ],
   entryComponents:[LoginComponent],
   providers: [DishService,PromotionService, LeadersService],
